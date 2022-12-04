9c828cfd86d1a150f288662eec270e4f32197342
Michelle Yue Wang <yue.7.wang@kcl.ac.uk>
2022-12-04 16:22:18 +0000

    Finish things-2

--- things/views.py
+++ things/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .forms import ThingForm
 
 def home(request):
-    from = ThingFrom()
+    form = ThingFrom()
     return render(request, 'home.html', {'form': form})
